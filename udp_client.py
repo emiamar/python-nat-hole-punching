9be2d650eb567b3a26c66a1f565c77061a27fde0
Amar Savalagi <emiamar@gmail.com>
2020-06-21 13:30:31 +0530

    Removed the logic from try loop

--- udp_client.py
+++ udp_client.py
@@ -15,15 +15,15 @@ def main(host='127.0.0.1', port=9999):
     )
     print(sock)
     sock.sendto(b'0', (host, port))
-    while True:
-        data, ran_server = sock.recvfrom(1024)
-        addr = msg_to_addr(data)
-        print(f'Received: Peer {addr} from RS {ran_server}')
-        sock.sendto(b'Mac', addr)
-        data, addr = sock.recvfrom(1024)
-        print(f'Received data from peer {addr}')
-        print(f'Data received : {data}')
-        return sock, addr
+
+    data, ran_server = sock.recvfrom(1024)
+    addr = msg_to_addr(data)
+    print(f'Received: Peer {addr} from RS {ran_server}')
+    sock.sendto(b'Mac', addr)
+    data, addr = sock.recvfrom(1024)
+    print(f'Received data from peer {addr}')
+    print(f'Data received : {data}')
+    return sock, addr
 
 
 if __name__ == '__main__':
